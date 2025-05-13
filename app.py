d3f318648578f1bba70886716ceec809c8168c6a
fahadsandhu1 <fahadmahmood004#gmail.com>
2025-05-13 18:10:15 +0000

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, This is fahad future devops engineer!"
+    return "Hello, This is fahad future devops engineer. INSHALLAH i will get success soon by the grace of ALLAH ALMIGHTY!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
